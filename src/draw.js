9f4bde3c7ccba72451758cc5bb1835440f18b66b
viraj <virajpatil2712@gmail.com>
2018-01-02 10:36:19 +0530

    Added functionality to fill the board with letters.

---
+++ src/draw.js
@@ -0,0 +1,33 @@
+let id = 1;
+
+const generateTable = function() {
+  let table = document.getElementById('board');
+  for (let i = 0; i < 7; i++) {
+    generateRow(table);
+  }
+  return table;
+}
+
+const generateRow = function(table) {
+  let row = document.createElement('tr');
+  for (let i = 0; i < 7; i++) {
+    let cell = document.createElement('td');
+    cell.id = id++;
+    row.appendChild(cell);
+  }
+  table.appendChild(row);
+}
+
+const fillBoard = function(){
+  let records = [];
+  let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  for (let i = 1; i < 50; i++) {
+    let index = Math.floor(Math.random() * letters.length);
+    if (!records.includes(letters[index]) || records.length > 25) {
+      document.getElementById(i).innerText = letters[index];
+      records.push(letters[index]);
+      i++;
+    }
+    i--;
+  }
+}
